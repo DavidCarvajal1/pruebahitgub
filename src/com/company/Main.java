cc8edd0ea13a3edc11327d58b4cbae2c8dcee364
dcarvajal <dcm2003caroti>
2021-11-16 10:47:32 +0100

    5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
     System.out.println("holaaaaaaaaaaaaa2");
-    System.out.println("holaaaaa 3");
+    System.out.println("holaa");
     }
 }
